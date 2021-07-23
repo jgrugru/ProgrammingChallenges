5fc7839b4512e69d6a1e5c04239939774632573f
jgrugru <jeff.gruenbaum@gmail.com>
2021-07-23 09:03:18 -0500

    added new challenge

---
+++ leetcode/longest_palindromic_substring.py
@@ -0,0 +1,38 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+
+def longestPalindrome(s: str) -> str:
+    largest_palindrome = s[0]
+    # l_index = 0
+    r_index =  len(s) - 1
+
+    for l_index in range(len(s)):
+        r_index = len(s) - 1
+        while (r_index - l_index) >= len(largest_palindrome):
+            # breakpoint()
+
+            if l_index == r_index:
+                break
+            elif s[l_index] == s[r_index]:
+                # print(f"r_index is {r_index}\ncurrent_str is {s[l_index:r_index+1]}\nis_palindrome --> {is_palindrome(s[l_index:r_index+1])}")
+                test_str = s[l_index:r_index+1]
+                if is_palindrome(test_str):
+                    if len(largest_palindrome) < len(test_str):
+                        largest_palindrome = test_str
+                        break
+                else:
+                    r_index -= 1
+            else:
+                r_index  -= 1
+
+    return largest_palindrome
+
+print(longestPalindrome("babad"))
+print(longestPalindrome("cbbd"))
+print(longestPalindrome("a"))
+print(longestPalindrome("ac"))
+print(longestPalindrome("fffjabbba"))
+print(longestPalindrome("dakkjkqekafklhadfddaa"))
+print(longestPalindrome("bbbbbbbbbbbbbbbbbbbbbbbbbbbbb"))
+#

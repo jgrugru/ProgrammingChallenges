7afa099845d6f73ae8c3a6c2c87cae0c79bbe591
jgrugru <jeff.gruenbaum@gmail.com>
2021-07-28 17:39:00 -0500

    modified challenge

--- leetcode/int_to_roman.py
+++ leetcode/int_to_roman.py
@@ -3,25 +3,27 @@ def intToRoman(num: int) -> str:
                       'L': 50, 'C': 100, 'D': 500,
                       'M': 1000}
     roman_numeral_list = list(reversed(roman_numerals.keys()))
-    modulus_dict = {}
     remainder = num
     returning_str = ""
+    previous_quotient = 0
 
     for count, letter in enumerate(reversed(roman_numerals)):
-        modulus_dict[letter], remainder = divmod(remainder, roman_numerals[letter])
+        quotient, remainder = divmod(remainder, roman_numerals[letter])
 
-        if modulus_dict[letter] < 4:
-            for x in range(modulus_dict[letter]):
-                returning_str += letter
-        else:
-            if modulus_dict[roman_numeral_list[count - 1]]:
+        if quotient == 4:
+            if previous_quotient:
                 returning_str = returning_str[0:len(returning_str)-1] + letter + roman_numeral_list[count - 2]
             else:
                 returning_str = returning_str[0:len(returning_str)] + letter + roman_numeral_list[count - 1]
+        else:
+            for x in range(quotient):
+                returning_str += letter
+           
+        previous_quotient = quotient
 
     return returning_str
 
 
-print(f"{intToRoman(999)}\t\t\tintToRoman(999)\nCMXCIX\t\t\t\tEXPECTED")
-print(f"{intToRoman(3999)}\t\t\tintToRoman(3999)\nMMMCMXCIX\t\t\t\tEXPECTED")
-print(f"{intToRoman(499)}\t\t\tintToRoman(499)\nCDXCIX\t\t\t\tEXPECTED")
+print(f"{intToRoman(999)}\nCMXCIX")
+print(f"{intToRoman(3999)}\nMMMCMXCIX")
+print(f"{intToRoman(499)}\nCDXCIX")
